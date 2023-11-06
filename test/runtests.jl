7276fa712d4a305d8474b4884dd33cc5b6c8c484
Daniel Sharp <dannys4@mit.edu>
2023-11-06 18:19:29 -0500

    Conform to Julia 1.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,16 @@ using Test
 include("test_map_creation.jl")
 include("test_map_eval.jl")
 @testset "TransportTestbed.jl" begin
-    @testset "Map Creation" TestMapCreation()
-    @testset "Map Evaluation" TestMapEvaluation()
-    @testset "Map Derivative" TestMapDerivative()
-    @testset "Map Second Derivative" TestMapSecondDerivative()
+    @testset "Map Creation" begin
+        TestMapCreation()
+    end
+    @testset "Map Evaluation" begin
+        TestMapEvaluation()
+    end
+    @testset "Map Derivative" begin
+        TestMapDerivative()
+    end
+    @testset "Map Second Derivative" begin
+        TestMapSecondDerivative()
+    end
 end
